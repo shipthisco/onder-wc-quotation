e1bccf643c883ce359a612be02350fa1b16f8d54
Mayur <mayur@gmail.com>
2019-02-01 13:05:42 +0530

    [Mayur] Missing header fix

--- src/app/services/quotation.service.ts
+++ src/app/services/quotation.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {UserMetaService} from './user.meta.service';
 
 
@@ -13,7 +13,8 @@ export class QuotationService {
     return new Promise((resolve, reject) => {
       this.http.post(
         this.userMetaService.organisation_api_endpoint + 'api/v1/public/incollection/third_party_quotation',
-        {'reqbody': quotation_data}
+        {'reqbody': quotation_data},
+        {headers: new HttpHeaders().set('organisation', this.userMetaService.organisation_id)}
       ).subscribe((response_data: any) => {
           if (response_data.success) {
             resolve(response_data.data);
